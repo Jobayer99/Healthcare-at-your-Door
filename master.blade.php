8f842cb5097a091a28255682d80a38df1c812da2
Jobayer99 <49040977+Jobayer99@users.noreply.github.com>
2019-05-03 14:21:01 +0600

    Add files via upload

---
+++ master.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Stay_Happy:)</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/aos.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/ionicons.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.timepicker.css') }}">
+
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    @yield('style')
+  </head>
+  <body>
+    <nav class="navbar py-4 navbar-expand-lg ftco_navbar navbar-light bg-light flex-row">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+    			<div class="col-lg-2 pr-4 align-items-center">
+		    		<a class="navbar-brand" href="index.html">Stay<span>Happy</span></a>
+	    		</div>
+	    		<div class="col-lg-10 d-none d-md-block">
+		    		<div class="row d-flex">
+			    		<div class="col-md-4 pr-4 d-flex topper align-items-center">
+			    			<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+						    <span class="text">Address: Bashundhara R/A</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">Email: counselling@gmail.com</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">Phone:+123456789</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </nav>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="{{ url('/') }}" class="nav-link pl-0">Home</a></li>
+	        	<li class="nav-item"><a href="{{ url('doctor') }}" class="nav-link">Doctor</a></li>
+	        	
+
+	        	
+	          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+            @if(Session::get('user_login'))
+	        	<li class="nav-item"><a href="{{ url('/profile') }}" class="nav-link">Profile</a></li>
+	        	<li class="nav-item"><a href="{{ url('/logout') }}" class="nav-link">Logout</a></li>
+	        	@else
+            <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link">Login/Signup</a></li>
+            @endif
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    <!-- END nav -->
+
+    @yield('content')
+
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-5">
+              <h2 class="ftco-heading-2 logo">Stay<span>Happy</span></h2>
+              <p>Stay happy by taking mental health councelling</p>
+            </div>
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Bashundhara R/A , Dhaka</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+123456789</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">counselling@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+
+	            <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-5">
+              <h2 class="ftco-heading-2">Recent Blog</h2>
+              <div class="block-21 mb-4 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> April, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="block-21 mb-5 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> April, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+
+
+          </div>
+          <div class="col-md">
+          	<div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Opening Hours</h2>
+            	<h3 class="open-hours pl-4"><span class="ion-ios-time mr-3"></span>We are open 24/7</h3>
+            </div>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p>Thank you</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/popper.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.easing.1.3.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.stellar.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/aos.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.animateNumber.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/bootstrap-datepicker.js') }}"></script>
+  <script src="{{ asset('frontend/js/jquery.timepicker.min.js') }}"></script>
+  <script src="{{ asset('frontend/js/scrollax.min.js') }}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{ asset('frontend/js/google-map.js') }}"></script>
+  <script src="{{ asset('frontend/js/main.js') }}"></script>
+  @yield('script')
+
+  </body>
+</html>
